76cdaf1715c54c08430083790137869971b141cd
uvaisho6 <tu786927@gmail.com>
2025-09-20 16:40:23 +0100

    refactor: update authOptions import paths

--- src/app/api/wallet/route.ts
+++ src/app/api/wallet/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
